f53e2cc5c3b9affbe4b4ca4d04e091ca50862302
SamhitReddy5 <samhitreddy5@gmail.com>
2026-04-29 01:10:22 +0530

    Initial commit - Snek

---
+++ Assets/Scripts/Snake.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Snake : MonoBehaviour
+{
+
+    Rigidbody2D rb;
+    public float moveSpeed;
+    public float rotateAmount;
+    float rot;
+    int score;
+    public GameObject LevelCompleteText;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            if(mousePos.x < 0)
+            {
+                rot = rotateAmount;
+            }
+            else
+            {
+                rot = -rotateAmount;
+            }
+
+            transform.Rotate(0, 0, rot);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = transform.up * moveSpeed;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Frogs")
+        {
+            Destroy(collision.gameObject);
+            score++;
+
+            if (score >= 15)
+            {
+                SceneManager.LoadScene("End");
+            }
+
+        }
+        else if (collision.gameObject.tag == "Eagles")
+        {
+            SceneManager.LoadScene("Game");
+        }
+    }
+
+    void LateUpdate()
+    {
+        if (transform.position.x > 15)
+            SceneManager.LoadScene(1);
+
+        if (transform.position.x < -15)
+            SceneManager.LoadScene(1);
+
+        if (transform.position.y > 25)
+            SceneManager.LoadScene(1);
+
+        if (transform.position.y < -25)
+            SceneManager.LoadScene(1);
+    }
+}
